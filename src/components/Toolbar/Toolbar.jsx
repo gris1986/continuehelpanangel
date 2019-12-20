d7589765c542131221e40e6a76af586b63aa4c20
gris1986 <grismolina1986@gmail.com>
2019-12-19 22:31:11 -0600

    Removed unused toolbar link. Changed anchors for Links.

--- src/components/Toolbar/Toolbar.jsx
+++ src/components/Toolbar/Toolbar.jsx
@@ -2,23 +2,26 @@ import React from 'react';
 
 import BurgerNavButton from '../BurgerNav/BurgerNavButton';
 import './Toolbar.css';
+import { Link } from 'react-router-dom';
 
-const toolbar = props => (
-  <header className="toolbar">
-    <nav className="toolbar_navigation">
-      <div className="toolbar_burger_button">
-        <BurgerNavButton click={props.burgerClickHandler} />
-      </div>
-      <div className="toolbar_logo"><a href="/">HELP AN ANGEL</a></div>
-      <div className="space_nav" />
-      <div className="toolbar_navigation_item">
-        <ul>
-          <li><a href="/sponsor">Sponsor a pet</a></li>
-          <li><a href="/about">About</a></li>
-        </ul>
-      </div>
-    </nav>
-  </header>
-);
+const toolbar = (props) => {
+  const { burgerClickHandler } = props;
+  return (
+    <header className="toolbar">
+      <nav className="toolbar_navigation">
+        <div className="toolbar_burger_button">
+          <BurgerNavButton click={burgerClickHandler} />
+        </div>
+        <div className="toolbar_logo"><Link to="/">HELP AN ANGEL</Link></div>
+        <div className="space_nav" />
+        <div className="toolbar_navigation_item">
+          <ul>
+            <li><Link to="/about">About</Link></li>
+          </ul>
+        </div>
+      </nav>
+    </header>
+  );
+};
 
 export default toolbar;
